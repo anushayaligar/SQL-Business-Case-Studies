aa9843fe4a9cc4ac76eeaaad2df568d7c2ca4b03
Anusha Arun Yaligar <yaligaranusha17@gmail.com>
2026-06-14 23:29:20 +0530

    Add files via upload

---
+++ Ecommerce-Case-Study/03_CTEs/02_Customer_Revenue_Contribution.sql
@@ -0,0 +1,22 @@
+-- Calculate each customer's revenue contribution percentage to the total company revenue.
+select * from orders;
+WITH customer_revenue AS
+(
+    SELECT customer_id,
+           SUM(total_amount) AS total_revenue
+    FROM orders
+    GROUP BY customer_id
+),
+total_company_revenue as(
+select sum(total_amount) as company_revenue
+from orders )
+select c.customer_id,
+cu.customer_name,
+c.total_revenue,
+ROUND(100.0 * c.total_revenue / t.company_revenue,2) AS revenue_percentage 
+from customer_revenue c
+join customers cu
+on c.customer_id=cu.customer_id
+cross join total_company_revenue t 
+order by c.total_revenue desc;
+
